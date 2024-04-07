71aef3771bb202b2aefc463512c3723529026c17
HARRIS KURNIADI SUMBOGO <harriskurniadi2@gmail.com>
2024-04-07 10:27:46 +0700

    fix base vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/gallery/',
   plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
